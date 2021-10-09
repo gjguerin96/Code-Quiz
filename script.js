2ee19d32949fa16f8156dd6251a44c4c24ae8515
Gabriel Guerin <gjguerin96@gmail.com>
2021-10-08 23:21:23 -0700

    added local storage javascript function

--- script.js
+++ script.js
@@ -6,49 +6,52 @@ var subtitle = document.querySelector("#subtitle")
 var check = document.querySelector("#check")
 var gameOverEl = document.querySelector("#game-over")
 var initials = document.querySelector("#initials")
-var finalScore = document.querySelector('#final-score');
+var finalScore = document.querySelector("#final-score")
+var submit = document.querySelector("#submit")
+var scores = document.querySelector("#scores")
+var scoreList = document.querySelector("#score-list")
+//var restart = document.querySelector("#restart")
 
+//database of questions and answers
 var questionList = [
     {
-        question: "What is the answer?", 
-        answers: [{text: "right", correct: true}, 
-        {text: "wrong1", correct: false}, 
-        {text: "wrong2", correct: false}, 
-        {text: "wrong3", correct: false}]
+        question: "Which language is used for styling?", 
+        answers: [{text: "CSS", correct: true}, 
+        {text: "Javascript", correct: false}, 
+        {text: "HTML", correct: false}, 
+        {text: "Spanish", correct: false}]
     },
     {
-        question: "another question?", 
-        answers: [{text: "wrong1", correct: false}, 
-        {text: "right", correct: true}, 
-        {text: "wrong2", correct: false}, 
-        {text: "wrong3", correct: false}]
+        question: "Which is a technique for planning the structure of your site?", 
+        answers: [{text: "Brushing", correct: false}, 
+        {text: "Wireframing", correct: true}, 
+        {text: "Jamming", correct: false}, 
+        {text: "Hamboning", correct: false}]
     },
     {
-        question: "a third question?", 
-        answers: [{text: "wrong1", correct: false}, 
-        {text: "wrong2", correct: false}, 
-        {text: "wrong3", correct: false}, 
-        {text: "right", correct: true}]
+        question: "What is used to enclose an array?", 
+        answers: [{text: "Quotes", correct: false}, 
+        {text: "Curly Brackets", correct: false}, 
+        {text: "Parentheses", correct: false}, 
+        {text: "Brackets", correct: true}]
     },
     {
-        question: "a fourth question?", 
-        answers: [{text: "wrong1", correct: false}, 
-        {text: "wrong2", correct: false}, 
-        {text: "wrong3", correct: false}, 
-        {text: "right", correct: true}]
+        question: "What is the name for true and false values?", 
+        answers: [{text: "Integer", correct: false}, 
+        {text: "Boolean", correct: true}, 
+        {text: "String", correct: false}, 
+        {text: "Class", correct: false}]
     },
     {
-        question: "a fifth question?", 
-        answers: [{text: "wrong1", correct: false}, 
-        {text: "wrong2", correct: false}, 
-        {text: "wrong3", correct: false}, 
-        {text: "right", correct: true}]
+        question: "What symbol is used to designate id?", 
+        answers: [{text: "Question Mark", correct: false}, 
+        {text: "Asterisk", correct: false}, 
+        {text: "Period", correct: false}, 
+        {text: "Pound/Hashtag", correct: true}]
     }
 ]
 
-//var correctTally = 0
-var display = 0
-
+//start button starts the timer and starts displaying questions
 startButton.addEventListener("click", function(event) {
     startTimer()
     getQuestions()
@@ -56,7 +59,6 @@ startButton.addEventListener("click", function(event) {
     h1El.classList.add("hide")
     subtitle.classList.add("hide")
     questions.removeAttribute('class')
-    console.log(display)
 })
 
 var timeLeft = 60
@@ -65,13 +67,16 @@ function startTimer() {
     timer = setInterval(function() {
         timeLeft--
         timerEl.textContent = timeLeft 
-        if (timeLeft===0) {
-            //clearInterval(timer)
+        if (timeLeft<=0) {
+            timeLeft = 0
+            timerEl.textContent = timeLeft
             gameOver()
         }
     },1000)
 }
 
+//ends the game once it has gone through all the questions
+var display = 0
 function nextQuestion() { 
     if (display === questionList.length) {
         questions.style.display = "none"
@@ -83,12 +88,11 @@ function nextQuestion() {
     }  
 }
 
+//displays the current question and its answers
 function getQuestions(){
     questions.innerHTML = ""
-    // questions.style.display = "block"
     var currentQuestion = questionList[display].question
     var h2El = document.createElement("h2")
-    //h2El.textContent = currentQuestion
     questions.appendChild(h2El)
     questions.children[0].textContent = currentQuestion
     var currentAnswers = questionList[display].answers
@@ -98,32 +102,55 @@ function getQuestions(){
         answerBtn.innerText = event.text
         h2El.appendChild(answerBtn)
         answerBtn.addEventListener("click", function() {
+            //checks whether the user's choice is correct and then moves on to the next question
             if (event.correct === true) {
                 check.textContent = "Correct!"
-                //correctTally++
             }
             else {
                 timeLeft = timeLeft - 10
                 check.textContent = "Incorrect!"
                 timerEl.textContent = timeLeft
-            } 
+            }
             display++
             nextQuestion()
         })
     }
 }
 
+//hides questions and answers and displays endgame info
 function gameOver () {
+    questions.style.display = "none"
+    check.style.display = "none"
     gameOverEl.removeAttribute('class')
     clearInterval(timer)
     finalScore.textContent = timeLeft
-    //gameOverEl.textContent = "Your final score: " + timeLeft + ". Enter your initials to save your score."
-    //initials.style.display = "block"
 }
 
-function saveScore(){
-    var initials =document.querySelector('#initials').value
-    console.log(initials)
-}
+submit.addEventListener('click', function (event) {
+    submit.classList.add("hide")
+    scores.classList.remove("hide")
+    var initials = document.querySelector('#initials').value.trim()
+    if (initials === "") {
+        return;
+    }
+    //var initialsScore = initials + " " + timeLeft
+    var playerData = {
+        playerName: initials, 
+        timeLeft: timeLeft
+    }
+    localStorage.setItem("playerData", JSON.stringify(playerData))
+    var fetch = JSON.parse(localStorage.getItem("playerData"))
+    scoreList.append(fetch.playerName, ", ", fetch.timeLeft)
+    // restart.addEventListener('click', function (event) {
+    //     scores.style.display = "none"
+    //     startButton.classList.remove("hide")
+    //     h1El.classList.remove("hide")
+    //     subtitle.classList.remove("hide")
+    //     timeLeft = 60
+    //     display = 0
+    //     timerEl.textContent = timeLeft
+    //     startTimer()
+    //     getQuestions()
+    // })
+})
 
-document.querySelector('#submit').addEventListener('click', saveScore)
