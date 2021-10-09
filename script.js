ad8dc45ee4c265bdc9dd9f937f7ef3342bb1c970
Gabriel Guerin <gjguerin96@gmail.com>
2021-10-08 20:45:47 -0700

    tutoring session. refined functions and structure

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var startButton = document.querySelector("#start-button")
 var timerEl = document.querySelector("#timer-text")
-var questions = document.querySelector(".questions")
-var wrongAnswer = document.querySelector(".wrong-answer")
-var rightAnswer = document.querySelector(".right-answer")
+var questions = document.querySelector("#questions")
 var h1El = document.querySelector("h1")
 var subtitle = document.querySelector("#subtitle")
 var check = document.querySelector("#check")
 var gameOverEl = document.querySelector("#game-over")
 var initials = document.querySelector("#initials")
+var finalScore = document.querySelector('#final-score');
 
 var questionList = [
     {
@@ -47,69 +46,84 @@ var questionList = [
     }
 ]
 
-var correctTally = 0
+//var correctTally = 0
 var display = 0
 
 startButton.addEventListener("click", function(event) {
     startTimer()
-    nextQuestion()
+    getQuestions()
     startButton.classList.add("hide")
     h1El.classList.add("hide")
     subtitle.classList.add("hide")
+    questions.removeAttribute('class')
+    console.log(display)
 })
+
 var timeLeft = 60
 function startTimer() {  
     
     timer = setInterval(function() {
         timeLeft--
-        timerEl.textContent = timeLeft + " seconds left"
+        timerEl.textContent = timeLeft 
         if (timeLeft===0) {
-            clearInterval(timer)
+            //clearInterval(timer)
             gameOver()
         }
     },1000)
 }
 
 function nextQuestion() { 
-    if (display >= 5) {
-        gameOver()
-        //questions.classList.add("hide")
-        return
+    if (display === questionList.length) {
+        questions.style.display = "none"
+        check.style.display = "none"
+        gameOver() 
     }
     else {
-        questions.innerHTML = ""
-        questions.style.display = "block"
-        var currentQuestion = questionList[display].question
-        var h2El = document.createElement("h2")
-        //h2El.textContent = currentQuestion
-        questions.appendChild(h2El)
-        questions.children[0].textContent = currentQuestion
-        var currentAnswers = questionList[display].answers
-        currentAnswers.forEach(makeButtons)
-        function makeButtons (event) {
-            answerBtn = document.createElement("button")
-            answerBtn.innerText = event.text
-            h2El.appendChild(answerBtn)
-            answerBtn.addEventListener("click", function() {
-                if (event.correct === true) {
-                    h2El.textContent += "Correct!"
-                    correctTally++
-                    console.log(correctTally)
-                }
-                else {
-                    timeLeft = timeLeft - 10
-                    check.textContent = "Incorrect!"
-                } 
-                display++
-                nextQuestion()
-            })
-        }
+        getQuestions()
     }  
 }
 
-function gameOver () {
-    questions.classList.add("hide")
-    timerEl.classList.add("hide")
-    gameOverEl.textContent = "Your final score: " + correctTally + ". Enter your initials to save your score."
-    initials.style.display = "block"
+function getQuestions(){
+    questions.innerHTML = ""
+    // questions.style.display = "block"
+    var currentQuestion = questionList[display].question
+    var h2El = document.createElement("h2")
+    //h2El.textContent = currentQuestion
+    questions.appendChild(h2El)
+    questions.children[0].textContent = currentQuestion
+    var currentAnswers = questionList[display].answers
+    currentAnswers.forEach(makeButtons)
+    function makeButtons (event) {
+        answerBtn = document.createElement("button")
+        answerBtn.innerText = event.text
+        h2El.appendChild(answerBtn)
+        answerBtn.addEventListener("click", function() {
+            if (event.correct === true) {
+                check.textContent = "Correct!"
+                //correctTally++
+            }
+            else {
+                timeLeft = timeLeft - 10
+                check.textContent = "Incorrect!"
+                timerEl.textContent = timeLeft
+            } 
+            display++
+            nextQuestion()
+        })
+    }
 }
+
+function gameOver () {
+    gameOverEl.removeAttribute('class')
+    clearInterval(timer)
+    finalScore.textContent = timeLeft
+    //gameOverEl.textContent = "Your final score: " + timeLeft + ". Enter your initials to save your score."
+    //initials.style.display = "block"
+}
+
+function saveScore(){
+    var initials =document.querySelector('#initials').value
+    console.log(initials)
+}
+
+document.querySelector('#submit').addEventListener('click', saveScore)
